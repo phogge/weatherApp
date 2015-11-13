dd0ce20f01938c3980965daa750190c6c69fa158
Tim Berry <timothyaberry@gmail.com>
2015-11-13 15:11:57 -0700

    updated states and bckgrd image to full URL

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,17 +4,17 @@ angular.module('weatherApp').config(function ($stateProvider, $urlRouterProvider
 		.state('home', {
 			url: '/home',
 			controller: 'homeCtrl',
-			templateUrl: './app/home/homeTmpl.html'
+			templateUrl: 'http://tberry30.github.io/weatherApp/app/home/homeTmpl.html'
 
 		}).state('forecast', {
 			url: '/forecast/:param1/:param2',
 			controller: 'forecastCtrl',
-			templateUrl: './app/forecast/forecastTmpl.html'
+			templateUrl: 'http://tberry30.github.io/weatherApp/app/forecast/forecastTmpl.html'
 
 		}).state('compare', {
 			url: '/compare',
 			controller: 'compareCtrl',
-			templateUrl: './app/compare/compareTmpl.html'
+			templateUrl: 'http://tberry30.github.io/weatherApp/app/compare/compareTmpl.html'
 
 		});
 
